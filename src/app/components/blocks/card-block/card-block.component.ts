3f63d9631ffdd6e2ccf794fb88786da31bdf474d
Ingmar Bergmann <ingmar.bergmann@gmail.com>
2020-03-22 23:21:35 +0100

    fixes and adaption

--- src/app/components/blocks/card-block/card-block.component.ts
+++ src/app/components/blocks/card-block/card-block.component.ts
@@ -9,7 +9,7 @@ export class CardBlockComponent {
   titleCard = 'Dezentrale Versorgung des Nötigsten';
   //numberCard = 8;
   subTitleCard = 'Wie kann man dezentrale Lösungen schaffen, damit die Menschen mit weniger Risiko an alle lebensnotwendigen Artikel kommen? Sie können lokale Erzeuger geholfen werden, damit Ihre Produkte direkt verkafut werden?';
-  contentCard = 'Der Supermarktbesuch ist aktuell eines der größten Risiken um sich selbst mit dem Virus zu infizieren oder um andere Menschen zu infizieren. Durch Lebensmittelautomaten kann der Kontakt zu anderen Menschen minimiert werden.';
+  contentCard = 'Der Supermarktbesuch ist aktuell eines der größten Risiken um sich selbst mit dem Virus zu infizieren oder um andere Menschen zu infizieren. Durch Lebensmittelautomaten kann der Kontakt zu anderen Menschen minimiert werden. Die Lösung wäre Lebenmittelautomaten bei lokalen Erzeuger. Schon jetzt bieten viele Bauern Ihre Produkte an und somit könnten sich Bürger in der Nähe dezentral versorgen. Durch Telemtrie-Daten kann man diese Daten abfragen und breitstellen.';
   angularImage1: string;
   angularImage2: string;
   angularImage3: string;
@@ -17,6 +17,6 @@ export class CardBlockComponent {
   constructor() {
     this.angularImage1 = '/assets/img/bauer.png';
     this.angularImage2 = '/assets/img/angular2.png';
-    this.angularImage3 = '/assets/img/angular2.png';}
+    this.angularImage3 = '/assets/img/bauer.png';}
 
 }
